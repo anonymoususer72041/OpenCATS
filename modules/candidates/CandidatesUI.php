cb109bf1e59e38b8737c547fd1b7817c7deaf82d
anonymoususer72041 <247563575+anonymoususer72041@users.noreply.github.com>
2026-02-12 11:00:00 +0100

    Stop persisting inline HTML for status changes

--- modules/candidates/CandidatesUI.php
+++ modules/candidates/CandidatesUI.php
@@ -3056,19 +3056,6 @@ class CandidatesUI extends UserInterface
 
             $activityNote = htmlspecialchars($activityNote);
 
-            // FIXME: Move this to a highlighter-method? */
-            if (strpos($activityNote, 'Status change: ') === 0)
-            {
-                foreach ($statusRS as $data)
-                {
-                    $activityNote = StringUtility::replaceOnce(
-                        $data['status'],
-                        '<span style="color: #ff6c00;">' . $data['status'] . '</span>',
-                        $activityNote
-                    );
-                }
-            }
-
             /* Add the activity entry. */
             $activityID = $activityEntries->add(
                 $candidateID,
