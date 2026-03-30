eae921fcd6f3c4cec4ff8e9ae4012993c28e3e18
anonymoususer72041 <247563575+anonymoususer72041@users.noreply.github.com>
2026-03-30 13:01:54 +0200

    Fix CareersUI candidate update argument alignment

--- modules/careers/CareersUI.php
+++ modules/careers/CareersUI.php
@@ -1590,8 +1590,8 @@ class CareersUI extends UserInterface
                 $candidateID, $candidate['isActive'] ? true : false, $firstName, $middleName,
                 $lastName, $email, $email2, $phoneHome, $phoneCell, $phone, $address, $address2, $city,
                 $state, $zip, $source, $keySkills, '', $employer, '', '', '', $candidate['notes'],
-                '', $bestTimeToCall, $automatedUser['userID'], $automatedUser['userID'], $gender,
-                $race, $veteran, $disability
+                '', $bestTimeToCall, $automatedUser['userID'], $candidate['isHot'] ? true : false,
+                $email, $email, $gender, $race, $veteran, $disability
             );
 
             /* Update extra feilds */
