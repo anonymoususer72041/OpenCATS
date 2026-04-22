36a61ebc4d03e291a739f1ec87e12777fcfd2bdd
anonymoususer72041 <247563575+anonymoususer72041@users.noreply.github.com>
2026-04-22 15:30:00 +0200

    Remove deprecated magic quotes checks

--- lib/fpdf/fpdf.php
+++ lib/fpdf/fpdf.php
@@ -908,10 +908,6 @@ function Image($file,$x,$y,$w=0,$h=0,$type='',$link='')
 			$type=substr($file,$pos+1, 3);
 		}
 		$type=strtolower($type);
-		$mqr=get_magic_quotes_runtime();
-        if (function_exists('set_magic_quotes_runtime')) {
-            set_magic_quotes_runtime(0);
-        }
 		if($type=='jpg' || $type=='jpeg' || $type=='php')
 			$info=$this->_parsejpg($file);
 		elseif($type=='png')
@@ -924,9 +920,6 @@ function Image($file,$x,$y,$w=0,$h=0,$type='',$link='')
 				$this->Error('Unsupported image type: '.$type);
 			$info=$this->$mtd($file);
 		}
-        if (function_exists('set_magic_quotes_runtime')) {
-            set_magic_quotes_runtime($mqr);
-        }
 		$info['i']=count($this->images)+1;
 		$this->images[$file]=$info;
 	}
@@ -1167,10 +1160,6 @@ function _putfonts()
 		$this->_out('<</Type /Encoding /BaseEncoding /WinAnsiEncoding /Differences ['.$diff.']>>');
 		$this->_out('endobj');
 	}
-	$mqr=get_magic_quotes_runtime();
-    if (function_exists('set_magic_quotes_runtime')) {
-        set_magic_quotes_runtime(0);
-    }
 	foreach($this->FontFiles as $file=>$info)
 	{
 		//Font file embedding
@@ -1208,9 +1197,6 @@ function _putfonts()
 		$this->_putstream($font);
 		$this->_out('endobj');
 	}
-    if (function_exists('set_magic_quotes_runtime')) {
-        set_magic_quotes_runtime($mqr);
-    }
 	foreach($this->fonts as $k=>$font)
 	{
 		//Font objects
